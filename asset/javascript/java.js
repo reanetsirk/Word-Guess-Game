3155f2e7227fe214fdbadf60d9e6df08db06ee8e
reanetsirk <reanetsirk@gmail.com>
2019-07-23 12:12:17 -0400

    best update but still not 100%

--- asset/javascript/java.js
+++ asset/javascript/java.js
@@ -9,67 +9,110 @@
     var guessesLeft = 9
     var lettersGuesses =[]
     var computerGuesses = []
-
-    var directionsText = document.getElementById("directions-text");
-    var userChoiceText = document.getElementById("userchoice-text");
-    var guessesRemaining = document.getElementById('guessesLeft')
-    var userGuesses = document.getElementById('userGuesses')
-    var winsText = document.getElementById("wins-text");
-    var lossesText = document.getElementById("losses-text");
+    var userGuess
    
+
+    Gamepad.onload = function(){
     var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-    
-    // create a function to reset the game
-
-    function resetGame() {
-        guessesLeft = 9;
-        guessesRemaining.textContent = "Guesses Left: " + guessesLeft;
-        computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-        console.log("reset computer guess=" + computerGuess);
-        lettersGuesses = []
-        userGuesses.textContent = "User guesses thus far: " + lettersGuesses;
+    computerGuesses.push(computerGuess);
+    console.log(computerGuesses[0]);
     }
-    
-    // this runs on every key press
-    document.onkeyup = function(event) {
-    // capture letter press in a variable
-    console.log("computer guess=" + computerGuess)
-    var userGuess = event.key;
-    console.log(userGuess)
-    // decrementing guesses left by 1 for every key press
-    guessesLeft--
-    guessesRemaining.textContent = "Guesses Left: " + guessesLeft; 
 
-    console.log(guessesLeft)
-    // push userGuess which is the letter pressed into the lettersGuessed array so the user can see 
-    // their past guesses
-     lettersGuesses.push(userGuess)
-     console.log(lettersGuesses)
+    document.onkeyup = function(event){
+        var userGuess = event.key;
+        lettersGuesses.push(userGuess);
+        console.log(computerGuesses[0]);
+    }
 
-     userGuesses.textContent = "User Guesses Thus Far: " + lettersGuesses.join(", ")
-
-    
-
-    if (userGuess === computerGuess) {
-        // increment wins variable
+    if((userGuess === computerGuesses[0]) && (guessesLeft > 0)) {
         wins++;
-
-
-        // write html to page
- winsText.textContent = "Wins: " + wins;
-        // reset game
-        resetGame();
-
+        guessesLeft = 9;
+        lettersGuesses.length = 0;
+        var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+        computerGuesses.push(computerGuess);
+        console.log(computerGuesses[0]);
     }
-
-    if (guessesLeft === 0) {
+    else if((userGuess!== computerGuesses[0]) && (guessesLeft > 0)) {
+        guessesLeft = guessesLeft-1;
+    }
+    else {
         losses++;
-        // write html to page
-        lossesText.textContent = "Losses: " + losses;
-        // reset game
-        resetGame();
+        guessesLeft = 9;
+        lettersGuesses.length = 0;
+        computerGuesses = 0;
+        var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+        computerGuesses.push(computerGuess);
+        console.log(computerGuesses[0]);
     }
-    }
+    var html = "<h3>So, you think your a Phychic, huh!</h3>" +
+          "<p>Wins-text: " + wins + "</p>" +
+          "<p>Losses-text: " + losses + "</p>" +
+          "<p>Guessesleft: " + guessesLeft + "</p>" +
+          "<p>UserGuesses: " + lettersGuesses + "</p>";
+ 
+document.querySelector("#game").innerHTML = html;
+
+
+//     var directionsText = document.getElementById("directions-text");
+//     var userChoiceText = document.getElementById("userchoice-text");
+//     var guessesRemaining = document.getElementById('guessesLeft')
+//     var userGuesses = document.getElementById('userGuesses')
+//     var winsText = document.getElementById("wins-text");
+//     var lossesText = document.getElementById("losses-text");
+   
+//     var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    
+//     // create a function to reset the game
+
+//     function resetGame() {
+//         guessesLeft = 9;
+//         guessesRemaining.textContent = "Guesses Left: " + guessesLeft;
+//         computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+//         console.log("reset computer guess=" + computerGuess);
+//         lettersGuesses = []
+//         userGuesses.textContent = "User guesses thus far: " + lettersGuesses;
+//     }
+    
+//     // this runs on every key press
+//     document.onkeyup = function(event) {
+//     // capture letter press in a variable
+//     console.log("computer guess=" + computerGuess)
+//     var userGuess = event.key;
+//     console.log(userGuess)
+//     // decrementing guesses left by 1 for every key press
+//     guessesLeft--
+//     guessesRemaining.textContent = "Guesses Left: " + guessesLeft; 
+
+//     console.log(guessesLeft)
+//     // push userGuess which is the letter pressed into the lettersGuessed array so the user can see 
+//     // their past guesses
+//      lettersGuesses.push(userGuess)
+//      console.log(lettersGuesses)
+
+//      userGuesses.textContent = "User Guesses Thus Far: " + lettersGuesses.join(", ")
+
+    
+
+//     if (userGuess === computerGuess) {
+//         // increment wins variable
+//         wins++;
+
+
+//         // write html to page
+//  winsText.textContent = "Wins: " + wins;
+//         // reset game
+//         resetGame();
+
+//     }
+
+//     if (guessesLeft === 0) {
+//         losses++;
+//         // write html to page
+//         lossesText.textContent = "Losses: " + losses;
+//         // reset game
+//         resetGame();
+//     }
+//     }
     // directionsText.textContent = "";
 
 
